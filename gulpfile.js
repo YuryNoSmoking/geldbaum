b0810441fb45867ed0a8d5a8c4df609a10089885
YuryNoSmoking <130525910+YuryNoSmoking@users.noreply.github.com>
2024-10-17 10:24:04 +0300

    gulp set

--- gulpfile.js
+++ gulpfile.js
@@ -9,33 +9,44 @@ const browserSync = require('browser-sync').create();
 function serve() {
   browserSync.init({
     server: {
-      baseDir: './dist'
-    }
+      baseDir: './dist',
+    },
   });
 }
 
-//задача копирования HTML файлов 
+//задача копирования HTML файлов
 function html() {
-  return gulp.src('src/**/*.html')   //то откуда галп будет брать файлы
-        .pipe(plumber()) //пайп на случай ошибок что бы сборка не падала
-				.pipe(gulp.dest('dist/'))  // пайп (метод) отправит файл в dist
-        .pipe(browserSync.reload({stream: true})); //пайп перезагрузит странцу в браузере после каждого изменения в файле
+  return gulp
+    .src('src/**/*.html') //то откуда галп будет брать файлы
+    .pipe(plumber()) //пайп на случай ошибок что бы сборка не падала
+    .pipe(gulp.dest('dist/')) // пайп (метод) отправит файл в dist
+    .pipe(browserSync.reload({ stream: true })); //пайп перезагрузит странцу в браузере после каждого изменения в файле
 }
 
-// задача склеивания css файлов 
+// задача склеивания css файлов
 function css() {
-  return gulp.src('src/blocks/**/*.css') // указали путь
-        .pipe(plumber())                 // вызвали пламбер
-        .pipe(concat('bundle.css'))      // вызвали конкат и склеили файлы
-				.pipe(gulp.dest('dist/')) // отправили результат в папку
-        .pipe(browserSync.reload({stream: true}));
+  return gulp
+    .src('src/blocks/**/*.css') // указали путь
+    .pipe(plumber()) // вызвали пламбер
+    .pipe(concat('bundle.css')) // вызвали конкат и склеили файлы
+    .pipe(gulp.dest('dist/')) // отправили результат в папку
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 //Перенос файлов картинок в dist/
 function images() {
-  return gulp.src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
+  return gulp
+    .src('src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}')
     .pipe(gulp.dest('dist/images'))
-    .pipe(browserSync.reload({stream: true}));
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+//Перенос видео в dist/
+function videos() {
+  return gulp
+    .src('src/videos/**/*.{mp4,webm,mov,avi}')
+    .pipe(gulp.dest('dist/videos'))
+    .pipe(browserSync.reload({ stream: true }));
 }
 
 //Очистка папки dist перед каждой новой сборкой
@@ -43,25 +54,26 @@ function clean() {
   return del('dist');
 }
 
-
 //задача слежения за файлами
 function watchFiles() {
   gulp.watch(['src/**/*.html'], html);
   gulp.watch(['src/blocks/**/*.css'], css);
   gulp.watch(['src/images/**/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch(['src/videos/**/*.{mp4,webm,mov,avi}'], videos);
 }
 
-//сборка одной командой build, 
+//сборка одной командой build,
 //series(выполняет задачи по очереди)
 // parallel(выполняет задачи параллельно)
-const build = gulp.series(clean, gulp.parallel(html, css, images));
+const build = gulp.series(clean, gulp.parallel(html, css, images, videos));
 //параллельное слежение за изменениями (отключить в терминале CONTROL + C)
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
 // эти строчки позволяют вызвать задачи из терминала
-exports.html = html; 
+exports.html = html;
 exports.css = css;
 exports.images = images;
+exports.videos = videos;
 exports.clean = clean;
 
 exports.build = build;
